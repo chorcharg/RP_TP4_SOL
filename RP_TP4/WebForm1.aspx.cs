685b1938cb1522ed9539ad863e21b043abebddc6
Jorge Mancinelli <jorgevmanci@gmail.com>
2024-04-20 14:21:44 -0300

    Agregar archivos de proyecto.

---
+++ RP_TP4/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace RP_TP4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        private const string NeptunoDB = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection Neptuno = new SqlConnection(NeptunoDB);
+            Neptuno.Open();
+
+
+        }
+    }
+}
