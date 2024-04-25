01da18a46d402d5875d24cf7f9794b563c14c043
Rockandboy <Rockandboy.fl@gmail.com>
2024-04-25 19:28:31 -0300

    Cambios A las consultas y carga modular del DataSet Y DDLS

--- RP_TP4/WebForm1.aspx.cs
+++ RP_TP4/WebForm1.aspx.cs
@@ -14,7 +14,8 @@ namespace RP_TP4
     {
 
         private const string ViajesDB = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True";
-        private const string consultaSql = "SELECT * FROM Provincias";
+        private const string consultaSqlProvincias = "SELECT * FROM Provincias";
+        private const string consultaSqlLocalidades = "SELECT * FROM Localidades"; 
 
         private DataSet setDatos = new DataSet();
 
@@ -25,35 +26,59 @@ namespace RP_TP4
             if (!IsPostBack)
             {
                 {
-
-                    CargarProvincias();
+                    //Cargo El dataset con todas las tablas 
+                    CargarTablas();
+                    //Agrego el item de Seleccione "X"
+                    DdlProvinciaInicio.Items.Add(new ListItem("--Seleccione Provincia--", "-1"));
+                    DdlProvinciaFinal.Items.Add(new ListItem("--Seleccione Provincia--", "-1"));
+                    DdlLocalidadInicio.Items.Add(new ListItem("--Seleccione Localidad--", "-1"));
+                    DdlLocalidadFinal.Items.Add(new ListItem("--Seleccione Localidad--", "-1"));
+                    //Cargo Los DropDownList
+                    CargarProvincias_Y_Localidades();
+                    
 
                 }
             }
         }
-        private void CargarProvincias()
+        private void CargarTablas()
         {
 
             SqlConnection sqlConnection = new SqlConnection(ViajesDB);
             sqlConnection.Open();
 
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consultaSql, sqlConnection);
-
+            //CARGO LA TABLA DE PROVINCIAS
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consultaSqlProvincias, sqlConnection);
             sqlDataAdapter.Fill(setDatos, "Provincias");
+
+            sqlDataAdapter = new SqlDataAdapter(consultaSqlLocalidades, sqlConnection);
+            sqlDataAdapter.Fill(setDatos, "Localidades");
             sqlConnection.Close();
+       
+        }
 
-
-
-            DdlProvinciaInicio.Items.Add(new ListItem("--Seleccione Provincia--", "-1"));
+        private void CargarProvincias_Y_Localidades()
+        {
             foreach (DataRow provincia in setDatos.Tables["Provincias"].Rows)
             {
                 DdlProvinciaInicio.Items.Add(
                     new ListItem(provincia["NombreProvincia"].ToString(), provincia["IdProvincia"].ToString())
                     );
 
+                DdlProvinciaFinal.Items.Add(
+                    new ListItem(provincia["NombreProvincia"].ToString(), provincia["IdProvincia"].ToString())
+                    );
             }
 
-
+            foreach (DataRow localidad in setDatos.Tables["Localidades"].Rows)
+            {
+                DdlLocalidadInicio.Items.Add(
+                    new ListItem(localidad["NombreLocalidad"].ToString(), localidad["IdLocalidad"].ToString())
+                    );
+                DdlLocalidadFinal.Items.Add(
+                    new ListItem(localidad["NombreLocalidad"].ToString(), localidad["IdLocalidad"].ToString())
+                    );
+            }
         }
+
     }
 }
